363034dede16c5c2c3a45eef19c03b4aee927782
abuhumayed <ahmedthanjim@hotmail.co.uk>
2024-10-07 15:07:45 +0100

    single

--- src/components/accordian/index.jsx
+++ src/components/accordian/index.jsx
@@ -1,9 +1,16 @@
 /* eslint-disable react/jsx-key */
 import { useState } from "react";
 import data from "./data";
+import "./styles.css";
 
 export default function Accordian() {
   const [selected, setSelected] = useState(null);
+
+  function handleSingleSelection(getCurrentId) {
+    console.log(getCurrentId);
+    setSelected(getCurrentId);
+  }
+  console.log(selected);
 
   return (
     <div className="wrapper">
@@ -11,10 +18,16 @@ export default function Accordian() {
         {data && data.length > 0 ? (
           data.map((dataItem) => (
             <div className="item">
-              <div className="title">
+              <div
+                className="title"
+                onClick={() => handleSingleSelection(dataItem.id)}
+              >
                 <h3>{dataItem.question}</h3>
                 <span>+</span>
               </div>
+              {selected === dataItem.id ? (
+                <div className="content">{dataItem.answer}</div>
+              ) : null}
             </div>
           ))
         ) : (
