f2c12a22407999a2d96f5ac837eab4580df6a05c
abuhumayed <ahmedthanjim@hotmail.co.uk>
2024-10-07 09:46:56 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Accordian from "./components/accordian";
+
+function App() {
+  return (
+    <>
+      <Accordian />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/accordian/index.jsx
@@ -0,0 +1,26 @@
+/* eslint-disable react/jsx-key */
+import { useState } from "react";
+import data from "./data";
+
+export default function Accordian() {
+  const [selected, setSelected] = useState(null);
+
+  return (
+    <div className="wrapper">
+      <div className="accordian">
+        {data && data.length > 0 ? (
+          data.map((dataItem) => (
+            <div className="item">
+              <div className="title">
+                <h3>{dataItem.question}</h3>
+                <span>+</span>
+              </div>
+            </div>
+          ))
+        ) : (
+          <div>No Data found!</div>
+        )}
+      </div>
+    </div>
+  );
+}
