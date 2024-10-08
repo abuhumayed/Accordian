a0a57b44f620a2d18281e618fe3f1b8868970c28
abuhumayed <ahmedthanjim@hotmail.co.uk>
2024-10-08 17:53:01 +0100

    finished

--- src/components/FAQ.jsx
+++ src/components/FAQ.jsx
@@ -1,9 +1,16 @@
 import Accordian from "./Accordian";
+import data from "../data";
 
 const FAQ = () => {
   return (
     <div className="bg-gray-200 p-4 rounded-lg">
-      <Accordian />
+      {data.map((dataItem) => (
+        <Accordian
+          question={dataItem.question}
+          answer={dataItem.answer}
+          key={dataItem.id}
+        />
+      ))}
     </div>
   );
 };
